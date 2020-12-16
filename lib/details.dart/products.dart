3a24b9644e37f83baeb48f7a05dffd05d6ae0f6f
marketmix-valenciad <duvan.valencia@inoutdelivery.com>
2020-12-16 14:26:54 -0500

    Actualizando textTheme

--- lib/details.dart/products.dart
+++ lib/details.dart/products.dart
@@ -7,19 +7,14 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Theme.of(context).primaryColor,
-      appBar: AppBar(
-        elevation: 0,
-        leading: Icon(
-          Icons.menu
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: <Widget>[ActionBarUser()],
         ),
-        actions: <Widget>[
-          ActionBarUser()
-        ],
-      ),
-      body: ProductsPageContainer(),
-      bottomNavigationBar: BottomNavigatorBarInApp(activeIndex:2)
-    );
+        body: ProductsPageContainer(),
+        bottomNavigationBar: BottomNavigatorBarInApp(activeIndex: 2));
   }
 }
 
@@ -27,27 +22,22 @@ class ProductsPageContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints  viewportConstraints){
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
         return SingleChildScrollView(
           child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: viewportConstraints.minHeight
-            ),
-            child: 
-            Container(
-              height: MediaQuery.of(context).size.height - 130,
-              child: Column(
-                children: <Widget>[
-                  SearcHere(),
-                  CategoriesFilter(),
-                  SizedBox( height: 15 ),
-                  Expanded(
-                    child: ProductsListTile()
-                  ),
-                ],
-              ),
-            )
-          ),
+              constraints:
+                  BoxConstraints(minHeight: viewportConstraints.minHeight),
+              child: Container(
+                height: MediaQuery.of(context).size.height - 130,
+                child: Column(
+                  children: <Widget>[
+                    SearcHere(),
+                    CategoriesFilter(),
+                    SizedBox(height: 15),
+                    Expanded(child: ProductsListTile()),
+                  ],
+                ),
+              )),
         );
       },
     );
@@ -62,35 +52,31 @@ class SearcHere extends StatefulWidget {
 class _SearcHereState extends State<SearcHere> {
   TextEditingController _controllerSearch;
 
-  void initState(){
+  void initState() {
     super.initState();
-    _controllerSearch = TextEditingController();  
+    _controllerSearch = TextEditingController();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left:20, right:20, top: 20, bottom: 20),
-      child: TextField(
-        onSubmitted: (string){
-
-        },
-        controller: _controllerSearch,
-        textAlign: TextAlign.start,
-        decoration: InputDecoration(
-          prefixIcon: Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Icon(Icons.search),
+        padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+        child: TextField(
+          onSubmitted: (string) {},
+          controller: _controllerSearch,
+          textAlign: TextAlign.start,
+          decoration: InputDecoration(
+            prefixIcon: Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Icon(Icons.search),
+            ),
+            //contentPadding: EdgeInsets.only(left: 30, right: 20, top: 20),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: new BorderSide(width: 2, color: Colors.red)),
+            labelText: 'Search here',
           ),
-          //contentPadding: EdgeInsets.only(left: 30, right: 20, top: 20),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15),
-            borderSide: new BorderSide(width: 2, color: Colors.red)
-          ),
-          labelText: 'Search here',
-        ),
-      )
-    );
-
+        ));
   }
 }
 
@@ -104,7 +90,7 @@ class CategoriesFilterState extends State<CategoriesFilter> {
 
   List<String> categories = ['Drinks', 'Cake', 'Coffe', 'Beer', 'Snakes'];
 
-  void setActive(index){
+  void setActive(index) {
     setState(() {
       active = index;
     });
@@ -113,33 +99,32 @@ class CategoriesFilterState extends State<CategoriesFilter> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left: 20),
-      height: 50,
-      width: MediaQuery.of(context).size.width,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (BuildContext context, int index){
-          return Padding(
-            padding: EdgeInsets.only(right:10),
-            child: RaisedButton(
-              elevation: 0,
-              color: active == index ? Theme.of(context).textSelectionColor : Theme.of(context).primaryColorLight,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12)
-              ),
-              onPressed: () => setActive(index),
-              child:  Text(
-                "${categories[index]}",
-                style: TextStyle(
-                  color: active == index ? Theme.of(context).primaryColorLight : Theme.of(context).textSelectionColor
-                ),
-              ),
-            )
-          ); 
-        }
-      )
-    );
+        padding: EdgeInsets.only(left: 20),
+        height: 50,
+        width: MediaQuery.of(context).size.width,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: RaisedButton(
+                    elevation: 0,
+                    color: active == index
+                        ? Theme.of(context).textSelectionColor
+                        : Theme.of(context).primaryColorLight,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    onPressed: () => setActive(index),
+                    child: Text(
+                      "${categories[index]}",
+                      style: TextStyle(
+                          color: active == index
+                              ? Theme.of(context).primaryColorLight
+                              : Theme.of(context).textSelectionColor),
+                    ),
+                  ));
+            }));
   }
 }
 
@@ -150,17 +135,15 @@ class ProductsListTile extends StatefulWidget {
 }
 
 class _ProductsListTileState extends State<ProductsListTile> {
-  int active; 
+  int active;
   final List<ModelProducts> products = List.generate(
-    10, 
-    (i) => ModelProducts(
-      img: 'thumbnail-default.png',
-      name: 'El nombre del producto $i',
-      price: '20.00'
-    )
-  );
+      10,
+      (i) => ModelProducts(
+          img: 'thumbnail-default.png',
+          name: 'El nombre del producto $i',
+          price: '20.00'));
 
-  void addToCart(index){
+  void addToCart(index) {
     setState(() {
       active = active == index ? products.length + 1 : index;
     });
@@ -168,59 +151,62 @@ class _ProductsListTileState extends State<ProductsListTile> {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: EdgeInsets.fromLTRB(12,10,12,5),
+    return Container(
+      padding: EdgeInsets.fromLTRB(12, 10, 12, 5),
       decoration: BoxDecoration(
-        color: Theme.of(context).primaryColorLight,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20)
-        )
-      ),
+          color: Theme.of(context).primaryColorLight,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
       //height: MediaQuery.of(context).size.height - 300,
       child: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (BuildContext context, int index){
-          return ListTile(
-            //leading: Icon(Icons.photo),
-            leading: FittedBox(
-              fit: BoxFit.fill,
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              //leading: Icon(Icons.photo),
+              leading: FittedBox(
+                fit: BoxFit.fill,
                 child: Container(
-                  width: 50,
-                  height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: new AssetImage('assets/thumbnail-default.png'),
-                    fit: BoxFit.fill
-                  )
-                )
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image:
+                                new AssetImage('assets/thumbnail-default.png'),
+                            fit: BoxFit.fill))),
               ),
-            ),
-            title: Text( products[index].name, style: Theme.of(context).textTheme.subhead.copyWith(fontWeight: FontWeight.w700  )),
-            subtitle: Text( '${products[index].price}',style: TextStyle( color: Theme.of(context).textSelectionColor, fontWeight: FontWeight.w500),),
-            trailing: FittedBox(
-              fit: BoxFit.fitHeight,
-              child: InkWell(
-                onTap: () => addToCart(index),
-                child: Container(
-                  child: Icon(
-                     active == index ? Icons.delete : Icons.add,
-                    color: active == index ? Theme.of(context).primaryColorLight : Colors.black45
-                  ),
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: active == index ? Theme.of(context).accentColor : Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                ),
-              )
-            ) ,
-          );
-        }
-      ),
+              title: Text(products[index].name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle1
+                      .copyWith(fontWeight: FontWeight.w700)),
+              subtitle: Text(
+                '${products[index].price}',
+                style: TextStyle(
+                    color: Theme.of(context).textSelectionColor,
+                    fontWeight: FontWeight.w500),
+              ),
+              trailing: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: InkWell(
+                    onTap: () => addToCart(index),
+                    child: Container(
+                      child: Icon(active == index ? Icons.delete : Icons.add,
+                          color: active == index
+                              ? Theme.of(context).primaryColorLight
+                              : Colors.black45),
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: active == index
+                              ? Theme.of(context).accentColor
+                              : Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                  )),
+            );
+          }),
     );
   }
 }
--- lib/resources/app.bottomNavigationBar.dart
+++ lib/resources/app.bottomNavigationBar.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_list/details.dart/detail.dart';
 import 'package:to_do_list/resources/button.dart';
 
-class BottomNavigatorBarInApp extends StatelessWidget{
+class BottomNavigatorBarInApp extends StatelessWidget {
   final int activeIndex;
-  List menuItems = new List.generate(
-    5, (i){
-      var indexOne = i+1;
-      if ( indexOne == 2 ){
-        return Text("s");
-      }else{
-        return Text("d");
-      }
-    }); 
   BottomNavigatorBarInApp({this.activeIndex});
-  goToLocarton(context, name){
-    Navigator.pushNamed(context, name);// Navigator.pushNamed(context, '/detail');
+  List menuItems = new List.generate(5, (i) {
+    var indexOne = i + 1;
+    if (indexOne == 2) {
+      return Text("s");
+    } else {
+      return Text("d");
+    }
+  });
+
+  goToLocarton(context, name) {
+    Navigator.pushNamed(
+        context, name); // Navigator.pushNamed(context, '/detail');
   }
 
   @override
   Widget build(BuildContext context) {
     return Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: Theme.of(context).primaryColor,
-        ),
-        child: Container(
+      data: Theme.of(context).copyWith(
+        canvasColor: Theme.of(context).primaryColor,
+      ),
+      child: Container(
           decoration: BoxDecoration(
             color: Theme.of(context).primaryColor,
           ),
@@ -34,45 +34,59 @@ class BottomNavigatorBarInApp extends StatelessWidget{
               topRight: Radius.circular(30.0),
             ),
             child: Container(
-              padding: EdgeInsets.fromLTRB(20, 5, 20 , 5),
-              color: Theme.of(context).primaryColorLight,
-              height: 50,
-              child:Row(
-                children: <Widget>[
+                padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                color: Theme.of(context).primaryColorLight,
+                height: 50,
+                child: Row(children: <Widget>[
                   Expanded(
-                    child: activeIndex == 1 ?
-                      ItemBottomNavigatorBarActive(text: 'Home', icon: Icons.home,) : 
-                      ItemBottomNavigatorInactive( icon: Icons.home, onTapFunction: () => goToLocarton(context, '/'),
-                    ),
+                    child: activeIndex == 1
+                        ? ItemBottomNavigatorBarActive(
+                            text: 'Home',
+                            icon: Icons.home,
+                          )
+                        : ItemBottomNavigatorInactive(
+                            icon: Icons.home,
+                            onTapFunction: () => goToLocarton(context, '/'),
+                          ),
                   ),
                   Expanded(
-                    child: activeIndex == 2 ?
-                      ItemBottomNavigatorBarActive(text: 'Store', icon: Icons.store,) : 
-                      ItemBottomNavigatorInactive( icon: Icons.store, onTapFunction: () => goToLocarton(context, '/products'),
-                    ),
+                    child: activeIndex == 2
+                        ? ItemBottomNavigatorBarActive(
+                            text: 'Store',
+                            icon: Icons.store,
+                          )
+                        : ItemBottomNavigatorInactive(
+                            icon: Icons.store,
+                            onTapFunction: () =>
+                                goToLocarton(context, '/products'),
+                          ),
                   ),
                   Expanded(
-                    child: activeIndex == 3 ?
-                      ItemBottomNavigatorBarActive(text: 'Info', icon: Icons.apps) : 
-                      ItemBottomNavigatorInactive( icon: Icons.apps, onTapFunction: () => goToLocarton(context, '/detail'),
-                    ),
+                    child: activeIndex == 3
+                        ? ItemBottomNavigatorBarActive(
+                            text: 'Info', icon: Icons.apps)
+                        : ItemBottomNavigatorInactive(
+                            icon: Icons.apps,
+                            onTapFunction: () =>
+                                goToLocarton(context, '/detail'),
+                          ),
                   ),
                   Expanded(
-                    child: activeIndex == 4 ?
-                      ItemBottomNavigatorBarActive(text: 'Cart', icon: Icons.shopping_cart) : 
-                      ItemBottomNavigatorInactive( icon: Icons.shopping_cart, onTapFunction: () => goToLocarton(context, '/'),
-                    ),
+                    child: activeIndex == 4
+                        ? ItemBottomNavigatorBarActive(
+                            text: 'Cart', icon: Icons.shopping_cart)
+                        : ItemBottomNavigatorInactive(
+                            icon: Icons.shopping_cart,
+                            onTapFunction: () => goToLocarton(context, '/'),
+                          ),
                   ),
-                ]
-              )
-            ),
-          )
-        ),
-      );
+                ])),
+          )),
+    );
   }
 }
 
-class ItemBottomNavigatorInactive extends StatelessWidget{
+class ItemBottomNavigatorInactive extends StatelessWidget {
   final IconData icon;
   final Function onTapFunction;
   ItemBottomNavigatorInactive({this.icon, this.onTapFunction});
@@ -82,10 +96,7 @@ class ItemBottomNavigatorInactive extends StatelessWidget{
       radius: 12,
       color: Theme.of(context).primaryColorLight,
       function: () => onTapFunction(),
-      child: Icon(
-        icon,
-        color: Theme.of(context).accentColor
-      ),
+      child: Icon(icon, color: Theme.of(context).accentColor),
       borderColor: Color(0x00000000),
       splashColor: Theme.of(context).accentColor,
       padding: EdgeInsets.all(2),
@@ -93,37 +104,33 @@ class ItemBottomNavigatorInactive extends StatelessWidget{
   }
 }
 
-class ItemBottomNavigatorBarActive extends StatelessWidget{
+class ItemBottomNavigatorBarActive extends StatelessWidget {
   final IconData icon;
   final String text;
   ItemBottomNavigatorBarActive({this.icon, this.text});
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: CustomRaisedButton(
-        radius: 12,
-        color: Theme.of(context).primaryColor,
-        function: (){},
-        child: FittedBox(
+        child: CustomRaisedButton(
+      radius: 12,
+      color: Theme.of(context).primaryColor,
+      function: () {},
+      child: FittedBox(
           child: Row(
-            children: <Widget>[
-              Icon(
-                icon,
-                color: Theme.of(context).accentColor,
-              ),
-              Text(
-                text,
-                style: TextStyle(
-                  color: Theme.of(context).accentColor
-                ),
-              )
-            ],
+        children: <Widget>[
+          Icon(
+            icon,
+            color: Theme.of(context).accentColor,
+          ),
+          Text(
+            text,
+            style: TextStyle(color: Theme.of(context).accentColor),
           )
-        ),
-        borderColor: Color(0x00000000),
-        splashColor: Theme.of(context).primaryColor,
-        padding: EdgeInsets.all(2),
-      )
-    );
+        ],
+      )),
+      borderColor: Color(0x00000000),
+      splashColor: Theme.of(context).primaryColor,
+      padding: EdgeInsets.all(2),
+    ));
   }
 }
