a6bad1ef98cf31d508f8ec38405b90e1f541e013
marketmix-valenciad <duvan.valencia@inoutdelivery.com>
2020-12-16 17:21:02 -0500

    Creando modelo para las categorias del home

---
+++ lib/Lists/Categories.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_first_app/Models/categories.dart';
+
+List<Category> homeCategories = [
+  Category(
+    name: 'Category 1',
+    description: 'Get the latest info about this',
+    cardColor: 'primary',
+  ),
+  Category(
+    name: 'Category 2',
+    description: 'Get noticed about our promotions',
+    cardColor: 'secondary',
+  ),
+  Category(
+    name: 'Category 3',
+    description: 'Get the latest info about this',
+    cardColor: 'primary',
+  ),
+];
---
+++ lib/Models/Categories.dart
@@ -0,0 +1,11 @@
+class Category {
+  final String name;
+  final String description;
+  final String cardColor;
+
+  Category({
+    this.name,
+    this.description,
+    this.cardColor,
+  });
+}
